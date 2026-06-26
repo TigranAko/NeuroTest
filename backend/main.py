d9f720fbb183f9f28fbb4def2480aaa73524e2fc
Tigran <tigranakopyan2007.ru@gmail.com>
2026-06-26 22:53:36 +0300

    feat(api): add lifespan with base structure

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
+from contextlib import asynccontextmanager
+
 from api.v1.routers import router_neurotest
 from fastapi import FastAPI
 from uvicorn import run
 
-app = FastAPI(title="NeuroTest")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Client
+    yield
+    # Close
+
+
+app = FastAPI(
+    title="NeuroTest",
+    liefespan=lifespan,
+)
 app.include_router(router_neurotest)
 
 if __name__ == "__main__":
