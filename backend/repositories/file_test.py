94ff40552c0091683dd34d3467c499ba933ce3bc
Tigran <tigranakopyan2007.ru@gmail.com>
2026-07-03 17:32:41 +0300

    feat(api): add FileTestRepository

---
+++ backend/repositories/file_test.py
@@ -0,0 +1,30 @@
+from uuid import UUID, uuid4
+
+from infrastructure.file_storage import FileStorage
+from repositories.interfaces import ITestRepository
+from schemas.test_output import TestOutput
+
+
+class FileTestRepository(ITestRepository):
+    def __init__(self):
+        self.storage = FileStorage()
+
+    async def add(self, test: TestOutput) -> UUID:
+        test_id = uuid4()
+        await self.storage.create_json(test_id, test)
+        return test_id
+
+    async def get(self, test_id: UUID) -> TestOutput:
+        data = await self.storage.read_json(test_id)  # read
+        test_output = TestOutput(**data)
+        return test_output
+
+    async def get_all(self) -> list[str]:
+        return await self.storage.get_files()
+
+    async def update(self, test_id: UUID, test: TestOutput) -> UUID:
+        file_data = await self.storage.create_json(test_id, test)
+        return file_data["file"]
+
+    async def delete(self, test_id: UUID) -> None:
+        await self.storage.delete_json(test_id)
