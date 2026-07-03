a44d720570a6a0f264994e764a22c3c889028cff
Tigran <tigranakopyan2007.ru@gmail.com>
2026-07-03 17:35:09 +0300

    feat(api): add handlers for Test object

--- backend/api/v1/routers/__init__.py
+++ backend/api/v1/routers/__init__.py
@@ -1,3 +1,4 @@
 from api.v1.routers.neurotest import router as router_neurotest
+from api.v1.routers.test import router as router_test
 
-__all__ = ["router_neurotest"]
+__all__ = ["router_neurotest", "router_test"]
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
-from api.v1.routers import router_neurotest
+from api.v1.routers import router_neurotest, router_test
 from fastapi import FastAPI
 from uvicorn import run
 
 app = FastAPI(title="NeuroTest")
 app.include_router(router_neurotest)
+app.include_router(router_test)
 
 if __name__ == "__main__":
     run(app=app, host="0.0.0.0", port=8000)
