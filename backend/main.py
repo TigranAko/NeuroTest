68eac3c581e914b73ccf23713493acb822328700
Tigran <tigranakopyan2007.ru@gmail.com>
2026-07-16 23:30:31 +0300

    refact(db): delete fastapi lifespan

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,3 @@
-from contextlib import asynccontextmanager
-
 from api.v1.routers import (
     router_answer,
     router_auth,
@@ -7,22 +5,10 @@ from api.v1.routers import (
     router_question,
     router_test,
 )
-from core.database import Base, engine
 from fastapi import FastAPI
-from models.user import User
 from uvicorn import run
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        print(User, "Добавляется таблица")
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-    await engine.dispose()
-
-
-app = FastAPI(title="NeuroTest", lifespan=lifespan)
+app = FastAPI(title="NeuroTest")
 app.include_router(router_auth)
 app.include_router(router_neurotest)
 app.include_router(router_test)
