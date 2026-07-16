9be35d12f685dd8ea7f559649597bff3f503fb5a
Tigran <tigranakopyan2007.ru@gmail.com>
2026-07-16 09:49:18 +0300

    feat(db): create tables in lifespan

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from api.v1.routers import (
     router_answer,
     router_auth,
@@ -5,10 +7,22 @@ from api.v1.routers import (
     router_question,
     router_test,
 )
+from core.database import Base, engine
 from fastapi import FastAPI
+from models.user import User
 from uvicorn import run
 
-app = FastAPI(title="NeuroTest")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as conn:
+        print(User, "Добавляется таблица")
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(title="NeuroTest", lifespan=lifespan)
 app.include_router(router_auth)
 app.include_router(router_neurotest)
 app.include_router(router_test)
