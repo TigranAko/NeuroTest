5a978577b33e85656c210f169c4bbd88a677581f
Tigran <tigranakopyan2007.ru@gmail.com>
2026-07-16 09:00:53 +0300

    feat(db): add user model

---
+++ backend/models/user.py
@@ -0,0 +1,11 @@
+from uuid import UUID, uuid4
+
+from core.database import Base
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    username: Mapped[str] = mapped_column(unique=True, index=True)
+    password: Mapped[str] = mapped_column()
