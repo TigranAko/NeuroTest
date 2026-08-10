9158f9bf57f2c61f73f74786e41a19c95d066d25
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-08-10 15:44:57 +0300

    fix(api): verify authorship for create and delete question

--- backend/api/v1/routers/question.py
+++ backend/api/v1/routers/question.py
@@ -3,6 +3,7 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends
 from schemas.question import QuestionCreate, QuestionResponse
+from services.jwt import get_current_user_id
 from services.question import QuestionService, get_question_service
 
 router = APIRouter(
@@ -14,10 +15,11 @@ router = APIRouter(
 @router.post("/tests/{test_id}/questions")
 async def create_question(
     question_service: Annotated[QuestionService, Depends(get_question_service)],
+    user_id: Annotated[UUID, Depends(get_current_user_id)],
     question: QuestionCreate,
     test_id: UUID,
 ) -> UUID:
-    return await question_service.add_question(test_id, question)
+    return await question_service.add_question(user_id, test_id, question)
 
 
 @router.get("/tests/{test_id}/questions")
@@ -39,6 +41,7 @@ async def read_question(
 @router.delete("/questions/{question_id}")
 async def delete_question(
     question_service: Annotated[QuestionService, Depends(get_question_service)],
+    user_id: Annotated[UUID, Depends(get_current_user_id)],
     question_id: UUID,
 ) -> dict[UUID, list[UUID]]:
-    return await question_service.delete_question(question_id)
+    return await question_service.delete_question(user_id, question_id)
