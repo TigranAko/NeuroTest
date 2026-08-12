d397a72b309331b5c37db357be9ea8a021fd6162
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-08-12 17:35:04 +0300

    fix(model): add position to answers

--- backend/models/answer.py
+++ backend/models/answer.py
@@ -1,8 +1,9 @@
+from enum import unique
 from typing import TYPE_CHECKING
 from uuid import UUID, uuid4
 
 from core.database import Base
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
@@ -15,5 +16,9 @@ class Answer(Base):
     question_id: Mapped[UUID] = mapped_column(ForeignKey("questions.id"))
     text: Mapped[str] = mapped_column()
     isCorrect: Mapped[bool] = mapped_column(default=False)
+    position: Mapped[int] = mapped_column()
 
     question: Mapped["Question"] = relationship(back_populates="answers")
+    __table_args__ = (
+        UniqueConstraint("question_id", "position", name="uq_answer_position"),
+    )
