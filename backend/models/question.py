7370849d0002ea42b0cc8f090dcf08ae6d4fea36
TigranAko <tigranakopyan2007.ru@gmail.com>
2026-08-13 08:24:28 +0300

    fix(model): add position to questions

--- backend/models/question.py
+++ backend/models/question.py
@@ -2,7 +2,7 @@ from typing import TYPE_CHECKING
 from uuid import UUID, uuid4
 
 from core.database import Base
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 if TYPE_CHECKING:
@@ -14,6 +14,10 @@ class Question(Base):
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     test_id: Mapped[UUID] = mapped_column(ForeignKey("tests.id"))
     text: Mapped[str] = mapped_column()
+    position: Mapped[int] = mapped_column()
 
     test: Mapped["Test"] = relationship(back_populates="questions")
     answers: Mapped[list["Answer"]] = relationship(back_populates="question")
+    __table_args__ = (
+        UniqueConstraint("test_id", "position", name="uq_question_position"),
+    )
